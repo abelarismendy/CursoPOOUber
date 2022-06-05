b1f2b1a2bf1f40954c807e2c70f5979fd5e574c7
abel-arismendy <abel@arismendy.co>
2022-06-05 14:13:49 -0500

    PHP finished

--- PHP/Payment.php
+++
@@ -1,4 +0,0 @@
-<?php
-class Account{
-    public $id = integer;
-}
--- PHP/index.php
+++ PHP/index.php
@@ -1,7 +1,12 @@
 <?php
+require_once("car.php");
+require_once("uberX.php");
+require_once("uberPool.php");
+require_once("account.php");
 
-require_once('Car.php');
-require_once('Account.php');
+$uberX = new UberX("CVB123", new Account("Abel Arismendy", "123456"), "Chevrolet", "Spark");
+$uberX->printDataCar();
 
-$car = new Car("ABC123", new Account("Abel Arismendy", "123456"));
-$car->printDataCar();
+$uberPool = new UberPool("TYU567", new Account("Angel Contreras", "654321"), "Dodge", "Attitude");
+$uberPool->printDataCar();
+?>
